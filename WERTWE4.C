0498446668578f1482c9ca35ab7d4e2895cbc472
karthikeyan2 <35687514+karthikeyan2@users.noreply.github.com>
2018-02-26 15:46:27 +0530

    Update WERTWE4.C

--- WERTWE4.C
+++ WERTWE4.C
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include<string.h>
 int main(void) {
-	char a[50];
-	int i,count=0;
-	gets(a);
-	
+	 char a[50];
+	 int i,count=0;
+	  gets(a);
+	 
 	for(i=0;a[i]!='\0';i++)
 	{
 		if(a[i] == ' ')
